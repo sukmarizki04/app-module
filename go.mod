4a010f2b577a135bb05d6ad037254ace72b71501
sukmarizki04 <sukmarizki04@gmail.com>
2023-07-31 01:09:21 +0700

    appmodule

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sukmarizki04/app-module.git
+
+go 1.20
+
+require github.com/sukmarizki04/project-gomodule/v2 v2.1.0 // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	projectmodule "github.com/sukmarizki04/project-gomodule/v2"
+)
+
+func main() {
+	n := projectmodule.Hello(" Rizki")
+	fmt.Println(n)
+}
